8211fa6745b230af1d08afc44c4502a2ab00e9da
Michael Kovalchuk <Michael.Kovalchuk@D2L.com>
2020-11-02 10:26:09 -0500

    Test download attribute

--- src/download.js
+++ src/download.js
@@ -1,11 +1,11 @@
 const URL = window.URL || window.webkitURL;
 
 export default async (url, name='download') => {
-	const res = await fetch(url);
-	const blob = await res.blob();
-	const blobUrl = URL.createObjectURL(blob);
+	// const res = await fetch(url);
+	// const blob = await res.blob();
+	// const blobUrl = URL.createObjectURL(blob);
 	const link = document.createElement('a');
 	link.download = name;
-	link.href = blobUrl;
+	link.href = url;
 	link.click();
 };
